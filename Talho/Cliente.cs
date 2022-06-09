2b8f19a083b7cd2675be0aac23de9c347ff2f9af
nuno <75589883+nuno-src@users.noreply.github.com>
2022-06-09 16:20:56 +0100

    Heranças
    Estrutura base para as classes cliente e gestor

---
+++ Talho/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talho
+{
+    
+    internal class Cliente : Pessoa
+    {
+        private int nif;
+        public Cliente()
+        {
+            nif = 0;
+        }
+
+        public Cliente(string nNome, string nEmail, Data nDataNAsc, int nNif)
+        {
+           
+            nome = nNome;
+            email = nEmail;
+            dataNasc = nDataNAsc;
+            nif = nNif;
+            
+        }
+        public int GetNif()
+        {
+            return nif;
+        }
+        public void SetNif(int nNif)
+        {
+            nif = nNif;
+        }
+
+
+
+
+    }
+}
---
+++ Talho/Gestor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talho
+{
+    internal class Gestor : Pessoa
+    {
+        private int numEmpregado;
+        public Gestor()
+        {
+            numEmpregado = 0;
+        }
+
+        public Gestor(string nNome, string nEmail, Data nDataNAsc, int nnumEmpregado)
+        {
+            nome = nNome;
+            email = nEmail;
+            dataNasc = nDataNAsc;
+            numEmpregado = nnumEmpregado;
+        }
+        public int GetNumEmpreg()
+        {
+            return numEmpregado;
+        }
+        public void SetGrau(int nnumEmpregado)
+        {
+            numEmpregado = nnumEmpregado;
+        }
+    }
+}
