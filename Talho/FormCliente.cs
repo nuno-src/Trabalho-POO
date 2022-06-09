4fc46702b5e34cc43cadfb98ac384b67c207ece4
jmarqs <94472809+jmarqs@users.noreply.github.com>
2022-06-09 18:43:59 +0100

    ListBox do Cliente

--- Talho/FormCliente.cs
+++ Talho/FormCliente.cs
@@ -7,10 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
 
 namespace Talho
 {
     //Form1 fgestor;
+
     public partial class FormCliente : Form
     {
 
@@ -18,7 +21,7 @@ namespace Talho
         {
             InitializeComponent();
 
-            //fPrincipal = fp;
+            //fg = fp;
         }
 
 
@@ -26,9 +29,43 @@ namespace Talho
         {
             //foreach (Produto p in fPrincipal.listBox1.Items)
             //{
-                
+
             //    listBox1.Items.Add(p);
             //}
+            if (File.Exists("dadosproduto.txt"))
+            {
+                FileStream fich = File.OpenRead("dadosproduto.txt");
+                FileStream chave = File.OpenRead("chave.key");
+
+                //AesCryptoServiceProvider cod = new AesCryptoServiceProvider();
+                Aes cod = Aes.Create();
+                BinaryReader brKey = new BinaryReader(chave);
+                cod.Key = brKey.ReadBytes(32);
+                cod.IV = brKey.ReadBytes(16);
+
+                CryptoStream cs = new CryptoStream(fich, cod.CreateDecryptor(), CryptoStreamMode.Read);
+
+                StreamReader sr = new StreamReader(cs);
+
+                string linha = sr.ReadLine();
+
+                while (linha != null)
+                {
+                    string[] auxP = linha.Split('|');
+                    string[] auxD = auxP[4].Split('/');
+
+                    Data d = new Data(int.Parse(auxD[0]), int.Parse(auxD[1]), int.Parse(auxD[2]));
+
+                    Produto p = new Produto(auxP[0], int.Parse(auxP[1]), double.Parse(auxP[2]), auxP[3], d);
+
+                    listBox1.Items.Add(p);
+
+                    linha = sr.ReadLine();
+                }
+                sr.Close();
+                fich.Close();
+                chave.Close();
+            }
         }
         private void button3_Click(object sender, EventArgs e)
         {
